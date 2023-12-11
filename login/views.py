c5199122503be4c5520bee4e0fa8d0c6ce2ba099
tih4r3-chan <ticampuoli18@gmail.com>
2023-12-11 20:58:54 -0300

    new
    Login listereque

--- login/views.py
+++ login/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 
 # Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,
+                                username = cd['username'],
+                                password = cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('El usuario esta autenticado')
+                else:
+                    return HttpResponse('El usuario esta inactivo')
+            else:
+                return HttpResponse('La información no es correcta')
+    else:
+        form = LoginForm()
+        return render(request, 'login.html', {'form':form})

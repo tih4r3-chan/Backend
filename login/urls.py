efc6139a8cd266b61733a64c4ec64103c50d09f5
tih4r3-chan <ticampuoli18@gmail.com>
2023-12-12 17:38:50 -0300

    new
    ah¿gregando login

--- login/urls.py
+++ login/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.contrib.auth import views as  auth_views
-from . import views
+# from . import views
+from .views import UserLoginView
 
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
+    path('login/', UserLoginView.as_view(), name='login'),
 ]
--- login/views.py
+++ login/views.py
@@ -1,25 +1,39 @@
-from django.shortcuts import render
-from .forms import LoginForm
-from django.contrib.auth import authenticate, login
-from django.http import HttpResponse
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
 
-# Create your views here.
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request,
-                                username = cd['username'],
-                                password = cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('El usuario esta autenticado')
-                else:
-                    return HttpResponse('El usuario esta inactivo')
-            else:
-                return HttpResponse('La información no es correcta')
-    else:
-        form = LoginForm()
-        return render(request, 'login.html', {'form':form})
+from .serializers import LoginSerializer
+
+class UserLoginView(generics.CreateAPIView):
+    serializer_class = LoginSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        # Autenticar al usuario
+        user = authenticate(
+            username=serializer.validated_data['username'],
+            password=serializer.validated_data['password']
+        )
+
+        if not user:
+            return Response(
+                {'error': 'Credenciales inválidas'},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        # Generar o recuperar el token de autenticación
+        token, created = Token.objects.get_or_create(user=user)
+
+        # Puedes personalizar la respuesta según tus necesidades
+        response_data = {
+            'token': token.key,
+            'message': 'Inicio de sesión exitoso',
+            # 'redirect_url': 'http://127.0.0.1:3000/login/',
+        }
+
+        headers = self.get_success_headers(response_data)
+        return Response(response_data, status=status.HTTP_200_OK, headers=headers)

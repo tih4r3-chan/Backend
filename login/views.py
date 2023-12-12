f7d69882dd15a051ec72a836241bb184f530c3b6
tih4r3-chan <ticampuoli18@gmail.com>
2023-12-12 19:17:40 -0300

    new
    cambios

--- login/views.py
+++ login/views.py
@@ -31,6 +31,7 @@ class UserLoginView(generics.CreateAPIView):
         # Puedes personalizar la respuesta según tus necesidades
         response_data = {
             'token': token.key,
+            'username': user.username,
             'message': 'Inicio de sesión exitoso',
             # 'redirect_url': 'http://127.0.0.1:3000/login/',
         }
